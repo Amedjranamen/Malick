847ab0b64d28bfa71346c5505adbcccc60591289
AMEDJRANA MBOULA Emmanuel Noé <amedjrananoe@gmail.com>
2026-05-15 01:55:11 +0200

    Use postgres as default database name if DB_NAME not specified

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ if (dbType === 'postgresql') {
         port: process.env.DB_PORT || 5432,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        database: process.env.DB_NAME || 'postgres',
         ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
     });
 } else {
